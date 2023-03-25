d9ec41741d87bc75a3373c5bd4c84092ee8a8ddf
Siva Prakash <prakashdeveloper@outlook.in>
2023-03-25 14:40:19 +0530

    Updated config/routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'todo_items#index'
+  resources :todo_items, only: [:create, :update, :destroy]
 end
